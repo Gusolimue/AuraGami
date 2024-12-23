87e167d4b3e44104736daf5d76ad9e5581e2e04b
NathanDude <nathan.fuentes002@gmail.com>
2024-12-23 12:45:59 -0600

    Pre-New Target Prefabs

---
+++ Assets/Scripts/TargetsAndObstacles/Spawner.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] targetsAndObstacles;
+    public Transform[] spawnPoints;
+    public float beatTime = (60/130)*2;
+    public float timer;
+
+    void Update()
+    {
+        if (timer>beatTime)
+        {
+            // Instantiate objects in random positions.
+            GameObject objects = Instantiate(targetsAndObstacles[Random.Range(0, 2)], spawnPoints[Random.Range(0, 4)]);
+
+            // Zeroing out objects in game.
+            objects.transform.localPosition = Vector3.zero;
+
+            // Will rotate spawnPoints (which will rotate objects) at different angles.
+            //objects.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+            timer -= beatTime;
+        }
+        timer += Time.deltaTime;
+    }
+}
