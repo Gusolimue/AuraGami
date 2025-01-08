2c0ce0810fe496f1a9ef3cc98e852de25ae4199a
NathanDude <nathan.fuentes002@gmail.com>
2025-01-08 13:54:20 -0600

    Scene Transition Fade working

--- Assets/Scripts/FrontEnd/FrontEnd.cs
+++ Assets/Scripts/FrontEnd/FrontEnd.cs
@@ -3,14 +3,33 @@ using UnityEngine.UI;
 
 public class FrontEnd : MonoBehaviour
 {
+    [SerializeField] GameObject playButton_BG_OnEnter;
+    public Animator playButton_BG_OnEnter_Anim;
+
+    private void Awake()
+    {
+        playButton_BG_OnEnter_Anim = playButton_BG_OnEnter.GetComponent<Animator>();
+        playButton_BG_OnEnter_Anim.enabled = false;
+    }
+
+    public void OnPlayButtonEnter()
+    {
+        playButton_BG_OnEnter_Anim.enabled = true;
+        playButton_BG_OnEnter_Anim.SetTrigger("OnPlayButtonEnter");
+    }
+    public void OnPlayButtonExit()
+    {
+        playButton_BG_OnEnter_Anim.SetTrigger("OnPlayButtonExit");
+    }
+
     public void OnPlayButtonPressed()
     {
         SceneTransitionManager.Instance.SceneTransitionSplash();
-        SceneMgr.Instance.SceneTransition();
     }
 
     public void OnLevelsButtonPressed()
     {
+        AudioManager.Instance.playSong(AudioManager.Instance.songs[1]);
         CanvasManager.Instance.ShowCanvasLevelSelect();
         Destroy(this.gameObject);
     }
--- Assets/Scripts/Managers/SceneTransitionManager.cs
+++ Assets/Scripts/Managers/SceneTransitionManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
 public class SceneTransitionManager : MonoBehaviour
@@ -6,14 +7,31 @@ public class SceneTransitionManager : MonoBehaviour
     public static SceneTransitionManager Instance;
     [SerializeField] Image transitionSplash;
 
+    public float fadeDuration = 10f;
+
     private void Awake()
     {
         Instance = this;
-        transitionSplash.gameObject.SetActive(false);
+        transitionSplash.color = new Color(transitionSplash.color.r, transitionSplash.color.g, transitionSplash.color.b,
+            0);
     }
 
     public void SceneTransitionSplash()
     {
-        transitionSplash.gameObject.SetActive(true);
+        StartCoroutine(TransitionFade(0));
+    }
+
+    public IEnumerator TransitionFade(float alpha)
+    {
+        alpha = 0f;
+
+        while (alpha < 1f)
+        {
+            alpha += Time.deltaTime / fadeDuration;
+            transitionSplash.color = new Color(transitionSplash.color.r, transitionSplash.color.g,
+                transitionSplash.color.b, alpha); // Updates alpha 
+            yield return null; // Wait for the next frame
+        }
+        SceneMgr.Instance.SceneTransition();
     }
 }
