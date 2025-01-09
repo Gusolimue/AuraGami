584ee5d94d85722d716ab864c5f1125180f80897
NathanDude <nathan.fuentes002@gmail.com>
2025-01-08 18:55:06 -0600

    Play Button BG changes color depending on pointer state

--- Assets/Scripts/FrontEnd/FrontEnd.cs
+++ Assets/Scripts/FrontEnd/FrontEnd.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.UI;
 
 public class FrontEnd : MonoBehaviour
 {
+    [Header("Play Button BG Assets")]
+    [SerializeField] Image playButton_BG;
     [SerializeField] GameObject playButton_BG_OnEnter;
     [SerializeField] GameObject playButton_BG_OnPressed;
-    public Animator playButton_BG_OnEnter_Anim;
-    public Animator playButton_BG_OnExit_Anim;
-    public Animator playButton_BG_OnPressed_Anim;
+
+    private Animator playButton_BG_OnEnter_Anim;
+    private Animator playButton_BG_OnExit_Anim;
+    private Animator playButton_BG_OnPressed_Anim;
+
+    public float colorChangeDuration = 3f;
 
     private void Awake()
     {
@@ -24,11 +30,14 @@ public class FrontEnd : MonoBehaviour
     {
         playButton_BG_OnEnter_Anim.enabled = true;
         playButton_BG_OnEnter_Anim.SetTrigger("OnPlayButtonEnter");
+        playButton_BG.color = new Color(1f, 1f, 1f, 1f);
+        //StartCoroutine(PlayButtonBGColorChangeOnEnter(.3f, .5f, .9f, .7f));
     }
     public void OnPlayButtonExit()
     {
         playButton_BG_OnExit_Anim.enabled = true;
         playButton_BG_OnExit_Anim.SetTrigger("OnPlayButtonExit");
+        playButton_BG.color = new Color(.3f, .5f, .9f, .7f);
     }
 
     public void OnPlayButtonPressed()
@@ -56,6 +65,26 @@ public class FrontEnd : MonoBehaviour
 
     public void OnQuitButtonPressed() // Will exit game (works for builds only).
     {
+        NewAudioManager.Instance.frontEndButtonSFX.Play();
         Application.Quit();
+    }
+
+    public IEnumerator PlayButtonBGColorChangeOnEnter(float red, float green, float blue, float alpha)
+    {
+        red = .3f;
+        green = .5f;
+        blue = .9f;
+        alpha = .7f;
+
+        while (red < 1f && green < 1f && blue < 1f && alpha < 1f)
+        {
+            red += Time.deltaTime / colorChangeDuration;
+            green += Time.deltaTime / colorChangeDuration;
+            blue += Time.deltaTime / colorChangeDuration;
+            alpha += Time.deltaTime / colorChangeDuration;
+
+            playButton_BG.color = new Color(red, green, blue, alpha); // Updates colors 
+            yield return null; // Wait for the next frame
+        }
     }
 }
