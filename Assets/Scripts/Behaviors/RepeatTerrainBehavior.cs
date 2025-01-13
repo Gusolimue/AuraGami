eb0e12ec0a3df2f2ee768f1751dfad2d408f35b4
Riley B <riley.blevins@g.austincc.edu>
2025-01-12 21:51:28 -0600

    Functional Mountain Terrain
    - Created "MountainLevel - Riley" scene.
    
    - Imported "Rocks and Terrains Pack - Low Poly" package.
    
    - Created "Green" Material.
    
    - Created "RepeatTerrainBehavior" script in the Script Behaviors folder.
    
    - Created a "Terrain" folder in the Prefabs folder.
    
    - Created "MountainTerrain_01" prefab in the Terrain folder.

---
+++ Assets/Scripts/Behaviors/RepeatTerrainBehavior.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+// Moves and repeats the terrain's position to appear like an infinite loop.
+public class RepeatTerrainBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float tmpSpeed = 300;
+
+    private Vector3 startPos;
+    private float repeatLength;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+        repeatLength = transform.localScale.z * 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.back * Time.deltaTime * tmpSpeed);
+
+        if (transform.position.z < startPos.z - repeatLength / 2)
+        {
+            transform.position = startPos;
+        }
+    }
+}
