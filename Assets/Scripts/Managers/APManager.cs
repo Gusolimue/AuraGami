fdcf5d0c9228bb3f4876c82fcd22ce9493a8ec51
NathanGuy02 <nathan.fuentes002@gmail.com>
2025-02-02 14:53:22 -0600

    Basic Implementation of AP and Sigils done
    - TargetInteractableBehavior script has two commented out lines for testing.
    - Tuning is necessary for the APManager script, but a rough implementation of it has been completed.

--- Assets/Scripts/Managers/APManager.cs
+++ Assets/Scripts/Managers/APManager.cs
@@ -3,18 +3,32 @@ using UnityEngine.UI;
 
 public class APManager : MonoBehaviour
 {
+    public static APManager Instance;
     [Header("Sigils")]
     [SerializeField] Slider sigilOne;
+    [SerializeField] Slider sigilTwo;
+    [SerializeField] Slider sigilThree;
     [Space]
+    [Header("Sigil Variables")]
     public float ap = .1f;
     public float curAP = 0f;
     public int apMultiplier = 1;
+
+    public int curSigil;
+
+    private void Awake()
+    {
+        Instance = this;
+        curSigil = 1;    
+    }
 
     public void IncreaseAP()
     {
         ap *= apMultiplier;
         curAP += ap;
-        sigilOne.value += ap;
+        if (curSigil == 1) sigilOne.value += ap;
+        if (curSigil == 2) sigilTwo.value += ap;
+        if (curSigil == 3) sigilThree.value += ap;
     }
 
     public void DecreaseAP()
@@ -24,12 +38,11 @@ public class APManager : MonoBehaviour
         sigilOne.value -= ap;
     }
 
-    public void APMult()
+    public void APBehavior()
     {
-        if (curAP <= 1)
-        {
-            apMultiplier++;
-        }
-    }
+        if (curAP <= 1) apMultiplier++;
 
+        if (sigilOne.value == 2) curSigil = 2;
+        if (sigilTwo.value == 2) curSigil = 3;
+    }
 }
