d99ee7aa3115899f7c62ca1e809dc860545e422c
Gus Thomas <124746451+Gusolimue@users.noreply.github.com>
2025-02-27 16:04:26 -0600

    trying to fix bug and heres some f

--- Assets/Scripts/Behaviors/BoardBehavior.cs
+++ Assets/Scripts/Behaviors/BoardBehavior.cs
@@ -22,21 +22,25 @@ public class BoardBehavior : MonoBehaviour
     void Update()
     {
         count += Time.deltaTime / movementSpeed;
-        if (mycount == 0) Debug.Log(count / 60f / LevelManager.Instance.level.soTrack.bpm);
+        //if (mycount == 0) Debug.Log(count / 60f / LevelManager.Instance.level.soTrack.bpm);
         if (currentBeat > 0)
         {
-            transform.position = Vector3.Lerp(lastPos, targetPos, count / ( 60f / LevelManager.Instance.level.soTrack.bpm ));
+            transform.position = Vector3.Lerp(lastPos, targetPos, count * ( LevelManager.Instance.level.soTrack.bpm /60));
         }
     }
     public void StartMovement()
     {
         currentBeat = 0;
+        originPos = this.transform.position;
         BeatManager.beatUpdated += UpdateMovementTarget;
     }
     void UpdateMovementTarget()
     {
         lastPos = transform.position;
-        targetPos = originPos + (Vector3.back * LevelManager.Instance.spawnDistance / LevelManager.Instance.beatsToPlayer) * currentBeat;
+        Vector3 tmpos;
+        tmpos = Vector3.back * LevelManager.Instance.spawnDistance;
+        Debug.Log(originPos);
+        targetPos = lastPos + (Vector3.back * LevelManager.Instance.spawnDistance / LevelManager.Instance.beatsToPlayer) * currentBeat;
         //Debug.Log(count);
         if (currentBeat > LevelManager.Instance.beatsToPlayer)
         {
