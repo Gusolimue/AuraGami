0c38e9779f99f8ef6833dcfb5abc388ee921bd76
NathanDude <nathan.fuentes002@gmail.com>
2025-02-26 19:05:15 -0600

    Current settings menu is not ideal

--- Assets/Scripts/Managers/LoadManager.cs
+++ Assets/Scripts/Managers/LoadManager.cs
@@ -40,7 +40,8 @@ public class LoadManager : MonoBehaviour
         {
             case eScene.frontEnd:
                 //CanvasManager.Instance.ShowCanvasFE();
-                CanvasManager.Instance.ShowCanvasLevelSelect();
+                //CanvasManager.Instance.ShowCanvasLevelSelect();
+                CanvasManager.Instance.ShowCanvasSettings();
                 AudioManager.Instance.PlayMusic(AudioManager.Instance.music_menu_titlescreen);
                 whichScene = 0;
                 break;
