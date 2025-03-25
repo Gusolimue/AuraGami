b38a8911d3290bdcb503d5910a50971a8eed418e
NathanGuy02 <nathan.fuentes002@gmail.com>
2025-03-25 15:56:55 -0500

    More mockup work

--- Assets/Scripts/Behaviors/ButtonBehaviors/LevelSelect_Buttons/ExplorationButton_Behavior.cs
+++ Assets/Scripts/Behaviors/ButtonBehaviors/LevelSelect_Buttons/ExplorationButton_Behavior.cs
@@ -4,7 +4,31 @@ public class ExplorationButton_Behavior : MonoBehaviour
 {
     [SerializeField] GameObject explorationPlanet;
 
-    private void OnExplorationButtonPressed()
+    private Animator planetAnim;
+    private Animator planetShrinkAnim;
+
+    public void Awake()
+    {
+       planetAnim = explorationPlanet.GetComponent<Animator>();
+       planetShrinkAnim = explorationPlanet.GetComponent <Animator>();
+
+       planetAnim.enabled = false;
+       planetShrinkAnim.enabled = false;
+    }
+
+    public void OnExplorationButtonEntered()
+    {
+        planetAnim.enabled = true;
+        planetAnim.SetTrigger("LevelHover");
+    }
+
+    public void OnExplorationButtonnExit()
+    {
+        planetShrinkAnim.enabled = true;
+        planetShrinkAnim.SetTrigger("LevelNotHover");
+    }
+
+    public void OnExplorationButtonPressed()
     {
         LevelSelectManager.Instance.whichLevel = 1;
         FrontEndSceneTransitionManager.Instance.SceneFadeInTransitionPauseSplash();
