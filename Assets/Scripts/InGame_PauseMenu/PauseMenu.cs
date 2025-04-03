fb2d31f1d7b3c2d8321707f51353097707b1981f
NathanGuy02 <nathan.fuentes002@gmail.com>
2025-04-03 13:07:50 -0500

    Tweaks

--- Assets/Scripts/InGame_PauseMenu/PauseMenu.cs
+++ Assets/Scripts/InGame_PauseMenu/PauseMenu.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using System.Collections;
 using UnityEngine;
 using EditorAttributes;
+using UnityEngine.UIElements;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -29,6 +30,7 @@ public class PauseMenu : MonoBehaviour
         //PauseManager.Instance.PauseGame(false); //Tmp
         PauseManager.Instance.yataAvatar.SetActive(true);
         PauseManager.Instance.naginiAvatar.SetActive(true);
+        PauseManager.Instance.progressBar.SetActive(true);
         PauseManager.Instance.StartCountdown();
         Destroy(this.gameObject);
     }
