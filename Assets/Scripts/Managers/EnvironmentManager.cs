26ba3f0182a297ed29ee802dba7e032d6a228292
RileyB-ACC <riley.blevins@g.austincc.edu>
2025-04-08 14:59:15 -0500

    Minor improvements to EnvironmentBehavior script
    - Merged "EnvironmentMoveAndDestroyBehavior" script into "EnvironmentBehavior" script to ensure synced movement.
    
    - Made minor improvements to "EnvironmentBehavior" script.
    
    - Still encountering a bug where there is a gap in environments.

--- Assets/Scripts/Managers/EnvironmentManager.cs
+++ Assets/Scripts/Managers/EnvironmentManager.cs
@@ -7,30 +7,54 @@ public class EnvironmentManager : MonoBehaviour
     [Header("Variables to Set")]
     [SerializeField] List<GameObject> rightPrefabs;
     [SerializeField] List<GameObject> leftPrefabs;
+    [SerializeField] int prefabLength; //300
 
     [Header("Variables to Adjust")]
-    public int spawnDistance;
-    [SerializeField] int centerOffset; // How far left/right the environment prefabs spawn
+    [SerializeField] int spawnDistance; //550
+    [SerializeField] int centerOffset; //100 // How far left/right the environment prefabs spawn
+    [SerializeField] int destroyBound; //-350
     [SerializeField] int environmentSpeed;
-    [SerializeField] int zBound;
+
+    [SerializeField] List<GameObject> environmentRows;
+
+    //private float startTime;
+    //public float movementTime;
 
     void Start()
     {
-        // Spawn the initial environment prefabs
-        SpawnEnvironment(-50);
-        SpawnEnvironment(spawnDistance-300);
+        // Spawn the initial three environment prefabs
+        SpawnEnvironment(spawnDistance - (prefabLength * 2));
+        SpawnEnvironment(spawnDistance - prefabLength);
         SpawnEnvironment(spawnDistance);
     }
 
+    // Controls environment movement and removal
+    void Update()
+    {
+        for (int rowIndex = 0; rowIndex < environmentRows.Count; rowIndex++)
+        {
+            GameObject eRow = environmentRows[rowIndex];
+            if (eRow.transform.position.z <= destroyBound)
+            {
+                Destroy(eRow);
+                environmentRows.RemoveAt(0);
+                SpawnEnvironment(spawnDistance);
+                rowIndex--;
+            }
+            eRow.transform.position = new Vector3(eRow.transform.position.x, eRow.transform.position.y, eRow.transform.position.z - Time.deltaTime * environmentSpeed);
+
+            //float tmpElapsedTime = Time.time - startTime;
+            //float t = 0f;
+            //t = Mathf.Clamp(tmpElapsedTime / movementTime, 0f, 1f);
+            //eRow.transform.position = new Vector3(eRow.transform.position.x, eRow.transform.position.y, Mathf.Lerp(spawnDistance, destroyBound, t));
+        }
+    }
+
     // Spawns the next row of environment prefabs at the set distance
-    public void SpawnEnvironment(int _spawnDistance)
+    private void SpawnEnvironment(int _spawnDistance)
     {
         GameObject environmentRow = new GameObject("EnvironmentRow");
         environmentRow.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + _spawnDistance);
-        environmentRow.AddComponent<EnvironmentMoveAndDestroyBehavior>();
-        environmentRow.GetComponent<EnvironmentMoveAndDestroyBehavior>().environmentManager = this;
-        environmentRow.GetComponent<EnvironmentMoveAndDestroyBehavior>().speed = environmentSpeed;
-        environmentRow.GetComponent<EnvironmentMoveAndDestroyBehavior>().destroyBound = zBound;
 
         Instantiate(rightPrefabs[Random.Range(0, rightPrefabs.Count)],
             new Vector3(environmentRow.transform.position.x + centerOffset, environmentRow.transform.position.y, environmentRow.transform.position.z),
@@ -39,5 +63,9 @@ public class EnvironmentManager : MonoBehaviour
         Instantiate(leftPrefabs[Random.Range(0, leftPrefabs.Count)],
             new Vector3(environmentRow.transform.position.x - centerOffset, environmentRow.transform.position.y, environmentRow.transform.position.z),
             Quaternion.identity, environmentRow.transform); // Spawn left prefab
+
+        environmentRows.Add(environmentRow);
+
+        //startTime = Time.time;
     }
 }
