439f486b2fd436a86546b30a2db13f83e84dc1ed
Gusolimue <gusolimue@gmail.com>
2025-04-16 03:13:59 -0500

    THREADED WORKING, AVATARS HUGE BUG

--- Assets/Materials/Shaders/FogFollow.cs
+++ Assets/Materials/Shaders/FogFollow.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class FogFollow : MonoBehaviour
 {
     public Camera cam;
-
+    public bool isEnabled;
     // Update is called once per frame
     void Update()
     {
         transform.position = cam.transform.position;
+    }
+    private void Awake()
+    {
+        GetComponent<MeshRenderer>().enabled = isEnabled;
     }
 }
--- Assets/Scripts/Circle/AvatarManager.cs
+++ Assets/Scripts/Circle/AvatarManager.cs
@@ -8,16 +8,16 @@ public class AvatarManager : MonoBehaviour
     [Header("Variables to Adjust")]
     public float playerDiameter;
     public float avatarDiameter;
-    public static float playerCircDiameter;
-    public static float avatarCircDiameter;
+    //float playerCircDiameter;
+    //float avatarCircDiameter;
 
     public bool DisableMovement;
 
     [Header("Variables to Set")]
-    [SerializeField]
-    RectTransform playerCircRectTransform;
-    [SerializeField]
-    RectTransform avatarCircRectTransform;
+    //[SerializeField]
+    //RectTransform playerCircRectTransform;
+    //[SerializeField]
+    //RectTransform avatarCircRectTransform;
 
     [SerializeField]
     Transform playerCircTransform;
@@ -34,6 +34,9 @@ public class AvatarManager : MonoBehaviour
     [SerializeField]
     public GameObject leftAvatar;
 
+    GameObject rightObject;
+    GameObject leftObject;
+
     [Header("Variables to Call")]
     public static AvatarManager Instance;
 
@@ -43,24 +46,33 @@ public class AvatarManager : MonoBehaviour
     private void Awake()
     {
         Instance = this;
-        playerCircDiameter = playerDiameter;
-        avatarCircDiameter = avatarDiameter;
+        GameObject tmpObject = Instantiate(new GameObject("Movement Targets"), this.transform);
+        rightObject = new GameObject("Right Movement Target");
+        leftObject = new GameObject("Left Movement Target");
+        rightObject.transform.SetParent(tmpObject.transform);
+        leftObject.transform.SetParent(tmpObject.transform);
+        playerCircTransform.localScale *= playerDiameter;
+        avatarCircTransform.localScale *= avatarDiameter;
     }
     private void Start()
     {
-        scaleMult = avatarCircDiameter / playerCircDiameter;
-        playerCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, playerCircDiameter);
-        playerCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, playerCircDiameter);
-        avatarCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, avatarCircDiameter);
-        avatarCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, avatarCircDiameter);
+        scaleMult = avatarDiameter / playerDiameter;
+        //playerCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, playerCircDiameter);
+        //playerCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, playerCircDiameter);
+        //avatarCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, avatarCircDiameter);
+        //avatarCircRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, avatarCircDiameter);
     }
     private void Update()
     {
         if(!DisableMovement)
         {
-            rightAvatar.transform.position = GetAvatarPos(true);
-            leftAvatar.transform.position = GetAvatarPos(false);
+            rightObject.transform.position = GetAvatarPos(true);
+            leftObject.transform.position = GetAvatarPos(false);
         }
+        rightAvatar.transform.localRotation = Quaternion.LookRotation(rightObject.transform.position + (Vector3.forward / 10), Vector3.up);
+        rightAvatar.transform.position = (rightObject.transform.position);
+        leftAvatar.transform.localRotation = Quaternion.LookRotation(leftObject.transform.position + (Vector3.forward / 10), Vector3.up);
+        leftAvatar.transform.position = (leftObject.transform.position);
     }
     //Given a bool to determine which controller. Returns a vector3 of the position the avatar will be set to that update
     Vector3 GetAvatarPos(bool right)
