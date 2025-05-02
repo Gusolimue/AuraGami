a996bde231cb3109b77656d7cc71793e63cc63de
Gusolimue <gusolimue@gmail.com>
2025-05-02 18:00:10 -0500

    avatarmanager comments

--- Assets/Scripts/Circle/AvatarManager.cs
+++ Assets/Scripts/Circle/AvatarManager.cs
@@ -14,7 +14,7 @@ public class AvatarManager : MonoBehaviour
     float playerCircDiameter;
     float avatarCircDiameter;
 
-    public eSide side;
+    //public eSide side;
 
     Color startColor;
     Color transparentColor;
@@ -112,14 +112,15 @@ public class AvatarManager : MonoBehaviour
             return tmpPos;
         }
     }
-
+    //Gus- this is the method that shows the evolution. it accepts a bool, which is determined by wether or not the player passes the stage. it then proceeds to do the start of the evolution, as that is the same wether the player passes or fails the level. then, it plays the pass or fail animation depending on the value of the bool. see A (pass) and B (fail) comments below
     IEnumerator CoEvolve(bool _pass)
     {
-
+        //Gus- here you will want to take control of the avatars away from the player
+        //Gus- and then you will want a while loop here to lerp the avatars together (close but not exactly on top of eachother, as you dont want them clipping through each other)
         yield return new WaitForSeconds(1f);
         float count = 0;
-        while (count < 1)
-        {
+        while (count < 1)//Gus- these while loops are just to wait for an amount of time within a coroutine without moving on. you can use these instead of "yield return new WaitForSeconds(float);" if theres code youd like to be running each frame the game is waiting. you will want to do things you only want to happen once outside of the loop (like instantiating or deleting avatars), because if they are inside the loop they will happen every frame until the time has passed
+        { //Gus- this while loop should be used just for changing the color of the sphere(after the avatars are already together, so the sphere is closing around them
             count += Time.deltaTime;
             // take control from player
 
@@ -138,6 +139,7 @@ public class AvatarManager : MonoBehaviour
             newRightAvatar.transform.SetParent(rightAvatar.transform.parent);
             newRightAvatar.transform.localPosition = rightAvatar.transform.localPosition;
 
+            
             Destroy(leftAvatar);
             Destroy(rightAvatar);
             // proceed with sphere fadeout / color change if fail
@@ -145,23 +147,26 @@ public class AvatarManager : MonoBehaviour
             yield return null;
         }
         count = 0;
-        yield return new WaitForSeconds(1f);
-        if (_pass)
+        yield return new WaitForSeconds(1f); //Gus- this wait is just to give more time for the sequence.
+        if (_pass)//Gus A - here is where the pass animation begins. if the variable is true, all it does is fade the orb back to transparency and reset the ap meter.
         {
+            //Gus- and then here, outside of the while loop and therefore after you have finished changing the color of the sphere, you can change out the old avatar prefabs for the next evolution. you dont want to do this earlier, because we want the models to stay the same if the player didnt pass.
             while (count < 1)
             {
                 count += Time.deltaTime;
                 evolveSphereRenderer.material.color = Color.Lerp(startColor, transparentColor, count / (60f / LevelManager.Instance.level.track.bpm) * 2);
                 yield return null;
             }
-            if (eSide.right == side)
-            {
-                APManager.Instance.ResetAP();
-            }
+            //Gus- made this quick change for you. this was only here because i was calling this method twice (one for each avatar) because we're only calling the method once now, we can always reset the ap on success
+            //if (eSide.right == side)
+            //{
+            //    APManager.Instance.ResetAP();
+            //}
+            APManager.Instance.ResetAP();
         }
         else
         {
-
+            //Gus B -  here is where the fail animation begins. its more complicated, but only slightly. 
             while (count < 1)
             {
                 count += Time.deltaTime;
@@ -175,14 +180,19 @@ public class AvatarManager : MonoBehaviour
                 evolveSphereRenderer.material.color = Color.Lerp(failColor, transparentColor, count / (60f / LevelManager.Instance.level.track.bpm) * 2);
                 yield return null;
             }
-            if (eSide.right == side)
-            {
-                CanvasManager.Instance.ShowCanvasStageFail();
-                PauseManager.Instance.isPaused = true;
-                BeatManager.Instance.PauseMusicTMP(true);
-            }
+            //Gus- see the comment about the similar change above
+            //if (eSide.right == side)
+            //{
+            //    CanvasManager.Instance.ShowCanvasStageFail();
+            //    PauseManager.Instance.isPaused = true;
+            //    BeatManager.Instance.PauseMusicTMP(true);
+            //}
+            CanvasManager.Instance.ShowCanvasStageFail();
+            PauseManager.Instance.isPaused = true;
+            BeatManager.Instance.PauseMusicTMP(true);
         }
     }
+    //Gus- Called by the stage manager at the end of a stages section in the music. the StagePassCheck method returns a bool based on if the player has enough points to pass the stage.
     public void StartEvolve()
     {
         StartCoroutine(CoEvolve(APManager.Instance.StagePassCheck()));
