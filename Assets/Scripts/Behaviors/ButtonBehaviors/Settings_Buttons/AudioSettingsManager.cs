15f446ca999b74a04e4af607cac39d7680b27f2d
NathanDude <nathan.fuentes002@gmail.com>
2025-05-08 19:50:38 -0500

    Constellation connectors functional

--- Assets/Scripts/Behaviors/ButtonBehaviors/Settings_Buttons/AudioSettingsManager.cs
+++ Assets/Scripts/Behaviors/ButtonBehaviors/Settings_Buttons/AudioSettingsManager.cs
@@ -3,9 +3,6 @@ using UnityEngine.UI;
 
 public class AudioSettingsManager : MonoBehaviour
 {
-    [SerializeField] public GameObject audioConstellationOn;
-    [SerializeField] public GameObject audioConstellationOff;
-
     [Header("Audio Sliders")]
     [SerializeField] public Slider masterAudioSlider;
     private float masterAudioSliderValue;
@@ -19,9 +16,6 @@ public class AudioSettingsManager : MonoBehaviour
 
     private void Awake()
     {
-        audioConstellationOn.SetActive(false);
-        audioConstellationOff.SetActive(true);
-
         masterAudioSlider.value = PlayerPrefs.GetFloat("save", masterAudioSliderValue);
         masterAudioSlider.onValueChanged.AddListener(MasterVolumeSlider);
 
@@ -30,19 +24,6 @@ public class AudioSettingsManager : MonoBehaviour
 
         sfxAudioSlider.value = PlayerPrefs.GetFloat("save", musicAudioSliderValue);
         sfxAudioSlider.onValueChanged.AddListener(SFXVolumeSlider);
-    }
-    public void OnAudioButtonEnter()
-    {
-        audioConstellationOn.SetActive(true);
-        audioConstellationOff.SetActive(false);
-        Debug.Log("Enter");
-    }
-
-    public void OnAudioButtonExit()
-    {
-        audioConstellationOn.SetActive(false);
-        audioConstellationOff.SetActive(true);
-        Debug.Log("Exit");
     }
 
     public void MasterVolumeSlider(float value)
--- Assets/Scripts/Behaviors/ButtonBehaviors/Settings_Buttons/SettingsButtonBehavior.cs
+++ Assets/Scripts/Behaviors/ButtonBehaviors/Settings_Buttons/SettingsButtonBehavior.cs
@@ -6,59 +6,30 @@ public class SettingsButtonBehavior : MonoBehaviour
 {
     [SerializeField] Slider[] connectors;
     public float connectorValueTarget;
-    public float fillSpeed = 100f;
+    public float fillSpeed = 1f;
 
-    private int connectorNum;
-    private bool isActive;
+    private bool isHovering;
 
-    private void Awake()
-    {
-        isActive = false;
-    }
 
     private void Update()
     {
-        for (connectorNum = 0; connectorNum < connectors.Length; connectorNum++)
+        foreach (Slider slider in connectors)
         {
-            if (isActive)
-            {
-                connectors[connectorNum].value = Mathf.Lerp(connectors[connectorNum].value, connectorValueTarget, Time.deltaTime * fillSpeed);
-            }
+            if (slider == null) continue;
+
+            float targetValue = isHovering ? 1f : 0f;
+            slider.value = Mathf.MoveTowards(slider.value, targetValue, fillSpeed * Time.deltaTime);
         }
     }
 
     public void IncreaseFill()
     {
-        StartCoroutine(FillConnectors());
+        isHovering = true;
     }
 
     public void DecreaseFill()
     {
-        StartCoroutine(UnfillConnectors());
+        isHovering = false;
     }
 
-    IEnumerator FillConnectors()
-    {
-        isActive = true;
-        Debug.Log("FILL");
-        connectorValueTarget = 1;
-        while (connectorNum <= 6)
-        {
-            connectorNum++;
-            yield return new WaitForSeconds(.2f);
-        }
-        isActive = false;
-    }
-
-    IEnumerator UnfillConnectors()
-    {
-        isActive = true;
-        connectorValueTarget = 0;
-        while (connectorNum >= 0)
-        {
-            connectorNum--;
-            yield return new WaitForSeconds(.2f);
-        }
-        isActive = false;
-    }
 }
