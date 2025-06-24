38e6ba18ddbf525a89ddb77fe3ed5ee8dfe28055
nnorrod <nnorrod@gmail.com>
2025-06-24 15:14:25 -0500

    Haptics Update
    - Added checks for haptics in hapticsmanager
    - Added prints for haptics in hapticsmanager
    - Added channel variable and set it to 0

--- Assets/Scripts/Managers/HapticsManager.cs
+++ Assets/Scripts/Managers/HapticsManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.XR;
 public class HapticsManager : MonoBehaviour
 {
     public static HapticsManager Instance;
+
+    uint channel = 0;
+    bool success;
+
     void Awake()
     {
         Instance = this;
@@ -11,6 +15,25 @@ public class HapticsManager : MonoBehaviour
 
     public void TriggerVibration(InputDevice controller, float amplitude, float duration)
     {
-        controller.SendHapticImpulse(1, amplitude, duration);
+        UnityEngine.XR.HapticCapabilities capabilities;
+        if (controller.TryGetHapticCapabilities(out capabilities))
+        {
+            if (capabilities.supportsImpulse)
+            {
+                success = controller.SendHapticImpulse(channel, amplitude, duration);
+                if (success)
+                {
+                    Debug.Log("Vibrating " + controller.name);
+                }
+                else
+                {
+                    Debug.Log("Failed to vibrate " + controller.name);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Device " + controller.name + " has no haptic capabilities");
+        }
     }
 }
