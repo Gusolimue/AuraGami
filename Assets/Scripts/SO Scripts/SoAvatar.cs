649660a4baa6d31a7cab365be77b5f2bec60c8f0
NathanDude <nathan.fuentes002@gmail.com>
2025-07-20 22:05:04 -0500

    Work on Color change

--- Assets/Scripts/SO Scripts/SoAvatar.cs
+++ Assets/Scripts/SO Scripts/SoAvatar.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
-
+public enum eEvo { firstEvolution, secondEvolution, thirdEvolution }
 [CreateAssetMenu(fileName = "New Avatar", menuName = "Create Avatar")]
 public class SoAvatar : ScriptableObject
 {
     public string avatarName;
     public GameObject[] AvatarPrefab;
     public GameObject CursorPrefab;
+    [NamedArray(typeof(eEvo))] public AvatarTextures[] avatarTextures;
+    [NamedArray(typeof(eEvo))] public Material[] avatarMats;
+
+}
+[System.Serializable]
+public class AvatarTextures
+{
+    public Texture[] textures;
 }
