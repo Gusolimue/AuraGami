3da80f06ef4d073d4bf8a64d02113956a25033ac
NathanGuy02 <nathan.fuentes002@gmail.com>
2025-07-22 15:21:26 -0500

    LevelProgress bug fixing

--- Assets/Scripts/Managers/LevelProgressManager.cs
+++ Assets/Scripts/Managers/LevelProgressManager.cs
@@ -16,38 +16,45 @@ public class LevelProgressManager : MonoBehaviour
 
     [Header("Progress Connectors")]
     [SerializeField] Slider progressConnector;
-    public float fillSpeed = 10f;
+    public float fillSpeed;
     public bool isProgress;
     public int curStage;
 
     public float targetValue;
     private float count;
+    public bool isCheat;
 
     private void Awake()
     {
         Instance = this;
-        curStage = LevelManager.currentStageIndex - 1;
+        curStage = LevelManager.currentStageIndex;
 
         if (curStage < 4) messageTXT.text = "Do You Continue?";
-        if (curStage == 1) StartCoroutine(StarColorChange(1));
-        if (curStage == 2) StartCoroutine(StarColorChange(2));
+        if (curStage == 1) StartCoroutine(StarColorChange(2));
+        if (curStage == 2) StartCoroutine(StarColorChange(3));
         if (curStage == 3) 
         {
-            StartCoroutine(StarColorChange(3));
+            StartCoroutine(StarColorChange(4));
+            messageTXT.text = "And You Continue...";
+        }
+        if (isCheat)
+        {
+            curStage = 4;
+            StartCoroutine(StarColorChange(4));
             messageTXT.text = "And You Continue...";
         }
     }
 
     private void Update()
     {      
-        if (curStage == 0)
+        if (curStage == 1)
         {
             targetValue = .34f;
             count += Time.deltaTime;
             progressConnector.value = Mathf.Lerp(progressConnector.value, targetValue, count / fillSpeed);
         }
 
-        if (curStage == 1)
+        if (curStage == 2)
         {
             targetValue = .68f;
             count += Time.deltaTime;
@@ -55,7 +62,14 @@ public class LevelProgressManager : MonoBehaviour
 
         }
 
-        if (curStage == 2)
+        if (curStage == 3)
+        {
+            targetValue = 1.02f;
+            count += Time.deltaTime;
+            progressConnector.value = Mathf.Lerp(progressConnector.value, targetValue, count / fillSpeed);
+        }
+
+        if (curStage == 4)
         {
             targetValue = 1.02f;
             count += Time.deltaTime;
@@ -65,11 +79,11 @@ public class LevelProgressManager : MonoBehaviour
     
     private IEnumerator StarColorChange(int starNum)
     {
-        yield return new WaitForSeconds(.5f);
+        yield return new WaitForSeconds(.8f);
         float alpha = 0f;
         float colorChangeDuration = 1f;
-       
-        if (starNum == 1)
+
+        if (starNum == 2)
         {
             while (alpha < 1f)
             {
@@ -77,9 +91,10 @@ public class LevelProgressManager : MonoBehaviour
                 stars[0].color = Color.Lerp(stars[0].color, starActive, alpha);
                 yield return null;
             }
+            alpha = 0;
         }
 
-        if (starNum == 2)
+        if (starNum == 3)
         {
             while (alpha < 1f)
             {
@@ -96,9 +111,11 @@ public class LevelProgressManager : MonoBehaviour
                 stars[1].color = Color.Lerp(stars[1].color, starActive, alpha);
                 yield return null;
             }
+            alpha = 0;
+            yield return new WaitForSeconds(.1f);
         }
 
-        if (starNum == 3)
+        if (starNum == 4)
         {
             while (alpha < 1f)
             {
