444de4fba7c5de4a5140cf648ba8fb3ac9ffb15e
Gus Thomas <124746451+Gusolimue@users.noreply.github.com>
2025-07-31 15:28:18 -0500

    presentation cam implementaiton

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.XR;
 
 public class CameraManager : MonoBehaviour
 {
     Camera[] myCams = new Camera[2];
-    
+    public float smoothing;
+
+    private void Awake()
+    {
+
+        XRSettings.gameViewRenderMode = GameViewRenderMode.None;
+    }
     void Start()
     {
-        myCams[0] = GameObject.Find("MainCamera").GetComponent<Camera>();
+        myCams[1] = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
-        myCams[1] = GameObject.Find("PresentationCamera").GetComponent<Camera>();
+        myCams[0] = GameObject.FindGameObjectWithTag("PresentationCamera").GetComponent<Camera>();
 
         Display.onDisplaysUpdated += OnDisplaysUpdated;
 
         mapCameraToDisplay();
     }
-
     void Update()
     {
-        
+        myCams[0].transform.rotation = Quaternion.Slerp(myCams[0].transform.rotation, myCams[1].transform.rotation, Time.deltaTime * smoothing);
     }
 
     void mapCameraToDisplay()
